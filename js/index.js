f18830b4ffba5a51e3246e05c051dd9a6f8f9522
braj011 <bhagyaraj01@gmail.com>
2018-10-13 13:31:00 +0100

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+var element = document.createElement('div');
+
+
+// did all the changes + changes in the console
+// based on the Lab / reading
